e56f21a9c79ad319af523b38d3227caf31a2fa1f
WmmyS <wesleymarcosm@gmail.com>
2022-05-15 11:26:56 -0300

    feat: Adicionada página de recuperação de dados.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,6 +9,14 @@ const routes: RouteRecordRaw[] = [
     ],
   },
 
+  {
+    path: '/music',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/MusicPage.vue') },
+    ],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
